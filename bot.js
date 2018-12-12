c70994ce80b064ac42ba505c63574130da9a50d1
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-12 23:48:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("520262638010368002");
+const mohamed= member.guild.channels.get("520565193810640897");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**# Welllllllcome , to ObX Kingdom 🌹 .**`), 4000)        
+setTimeout(() => mohamed.send(`**# Welllllllcome , to Nine 🌹 .**`), 4000)        
 }
 });
 
